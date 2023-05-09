211fbbd0abb80d949f8700709181638058d8b23c
Фируз Разаков <razackoff@gmail.com>
2023-05-09 13:16:08 +0500

    Добавить модель транзакции

--- Models/PurchaseTransaction.cs
+++ Models/PurchaseTransaction.cs
@@ -2,9 +2,8 @@
 {
 	public class PurchaseTransaction
 	{
-		public Guid TransactionID { get; set; }
+		public string ProductCategory { get; set; }
 		public string PhoneNumber { get; set; }
-		public string ProductName { get; set; }
 		public int InstallmentRange { get; set; }
 		public decimal ProductCost { get; set; }
 	}
