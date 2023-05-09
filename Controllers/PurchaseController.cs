7b0b07a58528e0c3ddb72a13bc439c31af679713
Фируз Разаков <razackoff@gmail.com>
2023-05-09 13:17:01 +0500

    Добавить контроллер вычисления суммы

---
+++ Controllers/PurchaseController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using test_task_from_alif.Models;
+using test_task_from_alif.Sevices;
+
+namespace test_task_from_alif.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PurchaseController : ControllerBase
+	{	
+		[HttpGet("GetPaymentAmount")]
+		public IActionResult GetPaymentAmount([FromQuery] PurchaseTransaction transaction)
+		{
+			PurchaseTransactionHandlerService purchaseTransactionHandlerService = 
+				new PurchaseTransactionHandlerService(transaction);
+
+			decimal sum = purchaseTransactionHandlerService.getSum();
+			var sender = new SendMessageService(transaction, sum);
+			sender.SendMessage();
+			return Ok(sum);
+		}
+	}
+}
