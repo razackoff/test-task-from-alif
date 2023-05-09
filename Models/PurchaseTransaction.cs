a07250d51b584b45ed440b185ca2b3380902c3dc
Фируз Разаков <razackoff@gmail.com>
2023-05-09 11:19:00 +0500

    Добавить класс транзакции.

---
+++ Models/PurchaseTransaction.cs
@@ -0,0 +1,12 @@
+﻿namespace test_task_from_alif.Models
+{
+	public class PurchaseTransaction
+	{
+		public Guid TransactionID { get; set; }
+		public string PhoneNumber { get; set; }
+		public string ProductName { get; set; }
+		public int InstallmentRange { get; set; }
+		public decimal ProductCost { get; set; }
+		public decimal Summa { get; set; }
+	}
+}
