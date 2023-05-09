3cb33611a6aaaa27ab5b66db7ed21b9ab24f7b7c
razac <razac@RAZAKOF>
2023-05-09 10:50:02 +0500

    Добавьте файлы проекта.

---
+++ Sevices/SendMessageService.cs
@@ -0,0 +1,11 @@
+﻿namespace test_task_from_alif.Sevices
+{
+	public class SendMessageService
+	{
+		public static void SendMessage(string phoneNumber, string sms)
+		{
+			Console.WriteLine(phoneNumber);
+			Console.Write(sms);
+		}
+	}
+}
